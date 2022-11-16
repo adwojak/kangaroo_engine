d27d1b78c35f802f3641ea95c5cd219f4ebc7821
adwojak <dwojaka207@gmail.com>
2022-11-16 15:08:08 +0100

    Working with pawn

--- piece/pawn.py
+++ piece/pawn.py
@@ -1,2 +1,33 @@
+from dev_helpers.board_printer import printer
+SIZE = 8
+
+
 class Pawn:
-    pass
+    is_white = False
+
+    vertical_position = 1
+    horizontal_position = 3
+
+    # def moving_algorithm(self):
+    #     if self.is_white:
+    #         pass
+    #     else:
+    #         self.vertical_position += 1
+
+    def get_standard_moves(self):
+        possible_moves = []
+        for vertical_move in range(self.vertical_position + 1, SIZE - self.vertical_position + 1):
+            possible_moves.append((vertical_move, self.horizontal_position))
+        return possible_moves
+
+    def get_attack_moves(self):
+        return [(self.vertical_position + 1, self.horizontal_position - 1), (self.vertical_position + 1, self.horizontal_position + 1)]
+
+    def calculate_moves(self):
+        moves = self.get_standard_moves()
+        moves.extend(self.get_attack_moves())
+        return moves
+
+
+pawn = Pawn()
+printer(pawn)
