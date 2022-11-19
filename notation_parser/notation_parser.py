7ce8f7dd460f06913922a62a964e638dea65a469
adwojak <dwojaka207@gmail.com>
2022-11-19 18:46:59 +0100

    Notation parser

--- notation_parser/notation_parser.py
+++ notation_parser/notation_parser.py
@@ -1,12 +1,59 @@
-from enum import StrEnum
+from enum import Enum, EnumMeta
 
 
-class Pieces(StrEnum):
-    # Pawn does not have notation letter
-    K = "King"
-    Q = "Queen"
-    B = "Bishop"
-    N = "Knight"
-    R = "Rook"
+class King:
+    # TMP
+    pass
 
 
+class Queen:
+    # TMP
+    pass
+
+
+class Bishop:
+    # TMP
+    pass
+
+
+class Knight:
+    # TMP
+    pass
+
+
+class Rook:
+    # TMP
+    pass
+
+
+class Pawn:
+    # TMP
+    pass
+
+
+class DefaultEnumMeta(EnumMeta):
+    def __getitem__(self, item):
+        try:
+            return super().__getitem__(item)
+        except KeyError:
+            return super().__getitem__("DEFAULT")
+
+
+class Pieces(Enum, metaclass=DefaultEnumMeta):
+    DEFAULT = Pawn
+    K = King
+    Q = Queen
+    B = Bishop
+    N = Knight
+    R = Rook
+
+
+class Move:
+    def __init__(self, white, piece):
+        self.white = white
+        self.piece = piece
+
+
+# aa = Pieces["a"]
+# print(aa)
+print(Pieces["a"])
