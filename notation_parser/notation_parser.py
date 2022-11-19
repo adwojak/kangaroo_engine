625fb5389b4b4d8c6788c446db98b9d1a1e93058
adwojak <dwojaka207@gmail.com>
2022-11-19 20:37:11 +0100

    Notation parser

--- notation_parser/notation_parser.py
+++ notation_parser/notation_parser.py
@@ -56,4 +56,26 @@ class Move:
 
 # aa = Pieces["a"]
 # print(aa)
-print(Pieces["a"])
+# print(Pieces["a"])
+# aaa = {'B', 'O', 'a', 'Q', '#', 'N', 'd', 'h', '+', 'R', 'g', 'e', 'x', 'c', 'f', 'b', '-', '=', 'K'}
+
+
+class NotationParser:
+    # def __init__(self, pgn_string):
+    #     self.pgn_string = pgn_string
+    #     self.pgn_list = pgn_string.split(" ")
+    #     print(self.pgn_list)
+
+    @staticmethod
+    def pgn_to_object(pgn_move):
+        sequence, move = pgn_move.split(".")
+        print(sequence)
+        print(move)
+
+
+with open("tests/sample2.txt") as file_stream:
+    pgn = file_stream.readline().replace("\n", "")
+# notation_parser = NotationParser(pgn)
+pgn_list = pgn.split(" ")
+print(pgn_list)
+NotationParser.pgn_to_object(pgn_list[0])
